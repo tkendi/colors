9e90bfefeb84a983c73c6a23af33dc52aac0adcd
ChayMyungjune <acg70159@gmail.com>
2023-05-01 21:11:23 +0900

    [UPDATE] Update media breakPoints setting

--- styles/stitches.config.ts
+++ styles/stitches.config.ts
@@ -1,7 +1,11 @@
 import { createStitches } from "@stitches/react";
-import { themes  } from "./theme";
+import { themes } from "./theme";
+import { generateMdeia } from "./breakPoints/createMedia";
 
 export const { theme, styled, css } = createStitches({
+  media: {
+    ...generateMdeia()
+  },
   theme: {
     color: {
       ...themes
